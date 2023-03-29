bf23c3093cf20b3e36fc4241565002b971618548
melhamdaoui811 <melhamdaoui811@gmail.com>
2023-03-29 10:27:49 +0000

    setup backend and next project

---
+++ tomato-fish/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'g3p5ejck',
+    dataset: 'production'
+  }
+})
---
+++ tomato-fish/sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'tomato-fish',
+
+  projectId: 'g3p5ejck',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
